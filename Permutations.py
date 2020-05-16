5aa91745165123d45f6703ecec53914f67b6ac2b
ASaltedF1sh <47856919+ASaltedF1sh@users.noreply.github.com>
2020-05-16 14:29:05 +0800

    DFS（递归）

--- Permutations.py
+++ Permutations.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     def permute(self, nums):
         n = len(nums)
         ans = []
@@ -14,4 +14,20 @@ class Solution:
         helper(0, [], list(range(len(nums))))
         
         return ans
+  
+#通过优化传参方式降低了空间复杂度
+class Solution2:
+    def permute(self, nums):   
+        n = len(nums)
+        ans = []
+
+        def helper(nums_, tmp):
+            if not nums_:
+                ans.append(tmp)
+                return
+            for k in range(len(nums_)):
+                helper(nums_[:k] + nums_[k+1:], tmp + [nums_[k]])
+        helper(nums, [])
+        return ans
+
  
