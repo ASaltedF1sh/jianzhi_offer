94828764ff6b6cce95a75c90f995051b1195c223
ASaltedF1sh <47856919+ASaltedF1sh@users.noreply.github.com>
2020-05-22 13:52:38 +0800

    回溯 + Memoization / DP

--- Distinct_Subsequences.py
+++ Distinct_Subsequences.py
@@ -37,3 +37,24 @@ class Solution:
                 self.helper(s[i + 1:], t, index + 1, map)
                 increment = self.ans - tmp
                 map[s[i + 1:] + str(index + 1)] = increment
+                
+#DP                
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        n1 = len(s)
+        n2 = len(t)
+        #前0个到前n个，故长度为n+1
+        dp = [[0] * (n1 + 1) for _ in range(n2 + 1)]
+        #dp[i][j]表示T的前i个字符构成的子序列在S的前j个字符构成的子序列中出现的次数
+        #空串是任何串的子串，所以dp[0][j] = 1
+        for j in range(n1 + 1):
+            dp[0][j] = 1
+        for i in range(1, n2 + 1):
+            for j in range(1, n1 + 1):
+                #t[i - 1]代表的是第i个字符，刚好与下面的dp[i][j]代表的前i个字符相对应
+                if t[i - 1] == s[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1]
+                else:
+                    dp[i][j] = dp[i][j - 1]
+        #print(dp)
+        return dp[-1][-1]
