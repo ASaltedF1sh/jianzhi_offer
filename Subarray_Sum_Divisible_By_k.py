c546746310e41616e5914a053651cd2077419c86
ASaltedF1sh <47856919+ASaltedF1sh@users.noreply.github.com>
2020-05-27 13:25:04 +0800

    前缀和 + 同余数性质

--- Subarray_Sum_Divisible_By_k.py
+++ Subarray_Sum_Divisible_By_k.py
@@ -1,3 +1,10 @@
+#同余数的性质：
+# 1.反身性：a≡a (mod m)；
+# 2.对称性：若a≡b(mod m)，则b≡a (mod m)；
+# 3.传递性：若a≡b(mod m)，b≡c(mod m)，则a≡c(mod m)；
+# 4.同余式相加：若a≡b(mod m)，c≡d(mod m)，则a  c≡b  d(mod m)；
+# 5.同余式相乘：若a≡b(mod m)，c≡d(mod m)，则ac≡bd(mod m)。
+
 class Solution:
     def subarraysDivByK(self, A: List[int], K: int) -> int:
         hashmap = {}
