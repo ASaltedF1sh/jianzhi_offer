2d6a6eb324ecccae386e190f545da989a7fc229e
ASaltedF1sh <47856919+ASaltedF1sh@users.noreply.github.com>
2020-05-31 23:16:34 +0800

    递归

---
+++ Symmetric_Tree.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isSymmetric(self, root):
+        if root == None:
+            return True
+
+        def helper(s, t):
+            if s == None and t == None:
+                return True
+            if s == None or t == None:
+                return False
+            if s.val != t.val:
+                return False
+            return helper(s.left, t.right) and helper(s.right, t.left)
+            
+        return helper(root.left, root.right)
