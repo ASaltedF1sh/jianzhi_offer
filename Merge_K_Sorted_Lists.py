a23f70569f3a9f6176d404367056d28363eabf1b
ASaltedF1sh <47856919+ASaltedF1sh@users.noreply.github.com>
2020-06-12 20:58:47 +0800

    堆

---
+++ Merge_K_Sorted_Lists.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        dummy = ListNode(None)
+        p = dummy
+        head = []
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(head, (lists[i].val, i))
+                lists[i] = lists[i].next
+        while head:
+            val, idx = heapq.heappop(head)
+            p.next = ListNode(val)
+            p = p.next
+            if lists[idx]:
+                heapq.heappush(head, (lists[idx].val, idx))
+                lists[idx] = lists[idx].next
+        return dummy.next
