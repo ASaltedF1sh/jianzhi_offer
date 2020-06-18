50645c76406a5afb499a0d0a53f9394fdfdac9d7
ASaltedF1sh <47856919+ASaltedF1sh@users.noreply.github.com>
2020-06-18 18:13:26 +0800

    Update Best_Time_To_Buy_And_Sell_Stock.py

--- dp/Best_Time_To_Buy_And_Sell_Stock.py
+++ dp/Best_Time_To_Buy_And_Sell_Stock.py
@@ -5,11 +5,7 @@ class Solution:
         days = len(prices)
         dp = [0] * days
         min_price = prices[0]
-
         for i in range(1, days):
-            if prices[i] - min_price > 0:
-                dp[i] = max(dp[i - 1], prices[i] - min_price)
-            else:
-                dp[i] = dp[i - 1]
+            dp[i] = max(dp[i - 1], prices[i] - min_price)
             min_price = min(min_price, prices[i])
         return max(dp)
