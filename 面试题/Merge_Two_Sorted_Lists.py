fa292bcf3a2c5de370385d41d9cb48fc578cdd4a
ASaltedF1sh <47856919+ASaltedF1sh@users.noreply.github.com>
2020-07-09 01:46:58 +0800

    合并两个有序链表

---
+++ 面试题/Merge_Two_Sorted_Lists.py
@@ -0,0 +1,29 @@
+#递归：
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        if l1.val <= l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+
+#循环
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode(None)
+        cur = dummy
+        while l1 and l2:
+            if l1.val <= l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        cur.next = l1 if l1 else l2
+        return dummy.next
