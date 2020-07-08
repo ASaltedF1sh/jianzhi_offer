daf36a1e8ba31cdca6b86e79c9baeba58019d237
ASaltedF1sh <47856919+ASaltedF1sh@users.noreply.github.com>
2020-07-09 03:07:39 +0800

    返回无重复元素数组的所有子集（包括空集）

---
+++ 面试题/Subsets.py
@@ -0,0 +1,10 @@
+class Solution2:
+    def subsets(self, nums):
+        res = []
+        n = len(nums)
+        def helper(i, tmp):
+            res.append(tmp)
+            for j in range(i, n):
+                helper(j + 1, tmp + [nums[j]])
+        helper(0, [])
+        return res  
